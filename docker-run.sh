7ff730abdce6b90270fe10c6414e16071032e9b1
Manan M <manan@theboco.in>
2025-10-03 13:47:29 -0400

    Update20 docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -9,29 +9,33 @@ function fixperms {
 }
 
 cd /opt/mautrix-telegram
-
 mkdir -p /data
 
-# Always regenerate and patch config
-if [[ -f /data/config.yaml ]]; then
-    rm -f /data/config.yaml
-fi
+# Force delete ANY old config
+rm -f /data/config.yaml /data/registration.yaml
 
-echo "Creating Telegram bridge config..."
+echo "Creating fresh Telegram bridge config..."
 cp example-config.yaml /data/config.yaml
 
-echo "Patching config with yq..."
+echo "Patching config..."
 
-# Use yq to properly modify YAML
-yq -i ".homeserver.address = \"${HOMESERVER_ADDRESS}\"" /data/config.yaml
-yq -i ".homeserver.domain = \"${HOMESERVER_DOMAIN}\"" /data/config.yaml
-yq -i ".appservice.database.type = \"postgres\"" /data/config.yaml
-yq -i ".appservice.database.uri = \"${DATABASE_URL}\"" /data/config.yaml
-yq -i ".telegram.api_id = ${TELEGRAM_API_ID}" /data/config.yaml
-yq -i ".telegram.api_hash = \"${TELEGRAM_API_HASH}\"" /data/config.yaml
+# Patch with yq - verify each command works
+yq eval -i ".homeserver.address = \"${HOMESERVER_ADDRESS}\"" /data/config.yaml
+yq eval -i ".homeserver.domain = \"${HOMESERVER_DOMAIN}\"" /data/config.yaml
+yq eval -i ".appservice.database.type = \"postgres\"" /data/config.yaml
+yq eval -i ".appservice.database.uri = \"${DATABASE_URL}\"" /data/config.yaml
+yq eval -i ".telegram.api_id = ${TELEGRAM_API_ID}" /data/config.yaml
+yq eval -i ".telegram.api_hash = \"${TELEGRAM_API_HASH}\"" /data/config.yaml
+yq eval -i ".logging.handlers.file.filename = \"/data/mautrix-telegram.log\"" /data/config.yaml
 
-# FIX LOGGING: Change log file path from /opt/mautrix-telegram (read-only) to /data (writable)
-yq -i ".logging.handlers.file.filename = \"/data/mautrix-telegram.log\"" /data/config.yaml
+# Verify database was actually set
+DB_TYPE=$(yq eval '.appservice.database.type' /data/config.yaml)
+echo "Database type set to: $DB_TYPE"
+
+if [[ "$DB_TYPE" != "postgres" ]]; then
+    echo "ERROR: Database config failed!"
+    exit 1
+fi
 
 echo "Config patched successfully!"
 
